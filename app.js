8a854b2549e0432b1a6315f7c0a18c0ed02291ea
Gary Wu <s19003045@gmail.com>
2019-10-20 12:11:23 +0800

    Add message for chat

--- app.js
+++ app.js
@@ -17,12 +17,43 @@ const linebotParser = bot.parser();
 
 bot.on('message', function (event) {
   console.log(event);
-
-  event.reply(event.message.text).then(function (data) {
-    // success
-  }).catch(function (error) {
-    // error
-  });
+  switch (event.message.text) {
+    case '你':
+      event.reply('我的主人，我肚子好餓，趕快餵我！')
+      break
+    case 'hi':
+      event.reply('我的主人，我肚子好餓，趕快餵我！')
+      break
+    case 'hello':
+      event.reply('我的主人，我肚子好餓，趕快餵我！')
+      break
+    case '天氣':
+      event.reply('不要管天氣了，我肚子好餓，趕快餵我！')
+      break
+    case '聽說':
+      event.reply('聽說什麼，你沒聽到我說我肚子好餓嗎？趕快餵我！')
+      break
+    case '問':
+      event.reply('問什麼不重要，重要是我肚子好餓，趕快餵我！')
+      break
+    case '說':
+      event.reply('說什麼說，不要再說了，我肚子好餓，趕快餵我！')
+      break
+    case '名字':
+      event.reply('我叫小雞，你是我的主人，你不知道嗎？我肚子好餓，趕快餵我！')
+      break
+    case '媽媽':
+      event.reply('你就是我媽，我肚子好餓，趕快餵我！')
+      break
+    default:
+      event.reply('不要再說了，我肚子好餓，趕快餵我！')
+  }
+  if (event.message.text = )
+    event.reply(event.message.text).then(function (data) {
+      // success
+    }).catch(function (error) {
+      // error
+    });
 });
 
 
